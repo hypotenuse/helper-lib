8344857dfaade3e47b245eb8e0ad2c9bfcc2c44c
Ivan Stern <hypotenuse@yandex.ru>
2018-03-10 23:56:01 +0300

    Delete langbox.js

--- lgc/langbox.js
+++
@@ -1,8 +0,0 @@
-;(function(_) {
-	if (common.cookie.get('i18n') == null) {
-		// show langBox after 700ms
-		_.setTimeout(
-			'common.dom.addClass(common.dom.get(\'langBox\'), \'show\')', 700
-		)
-	}
-})(window);
